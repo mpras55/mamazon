2a6f49b14e44cb5653db8f076c22dc9ebb7a4a63
mpras55 <manjunath.bp@gmail.com>
2018-04-17 04:07:27 -0600

    Stage 1

---
+++ mamazonCustomer.js
@@ -0,0 +1,111 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "password",
+	database: "mamazonDB"
+});
+
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log("\nWelcome to Mamazon!");
+	getSaleOrder();
+});
+
+function getSaleOrder() {
+	console.log("Following are the available items");
+	connection.query("SELECT department_name AS 'Department', product_name AS 'Product', product_id AS 'Product Code', price AS 'Price' FROM products WHERE stock_qty > 0 ORDER BY 1,2,3 ASC", function (err, res) {
+		if (err) throw err;
+		// Log all results of the SELECT statement
+		console.table(res);
+		inquirer
+			.prompt([
+				{
+					type: "input",
+					message: "Enter the product code that you want to purchase:",
+					name: "prodcode"
+				},
+				{
+					type: "input",
+					message: "How many units do you want to purchase?",
+					name: "quantity"
+				}
+			])
+			.then(function (orderResponse) {
+				connection.query("SELECT stock_qty, price FROM products where product_id=?", [orderResponse.prodcode], function (err, res) {
+					if (err) {
+						throw err;
+					}
+					if (res.length > 0) {
+						// console.log("Existing inventory: " + res[0].stock_qty);
+						if (res[0].stock_qty >= orderResponse.quantity) {
+							console.log("Processing Order...");
+							processOrder(orderResponse.prodcode, orderResponse.quantity, res[0].price, res[0].stock_qty);
+						} else {
+							console.log("Insufficient inventory. Only " + res[0].stock_qty + " remaining in stock.");
+							salesPrompt();
+						}
+					} else {
+						console.log("Not an existing product code");
+						salesPrompt();
+					}
+				});
+			});
+	});
+}
+
+function processOrder(prod, qty, price, inventory) {
+	var prodCode = prod;
+	var prodQty = qty;
+	var prodPrice = price;
+	var prodInventory = inventory;
+
+	// console.log("You are buying product code " + prodCode + " Quantity: " + prodQty + " at price of " + prodPrice + " existing stock: " + prodInventory);
+
+	var saleAmount = (prodQty * prodPrice).toFixed(2);
+	var newInventory = prodInventory - prodQty;
+
+	// console.log("Sale amount: " + saleAmount + "updated inventory: " + newInventory);
+	connection.query(
+		"UPDATE products SET ? WHERE ?",
+		[
+			{
+				stock_qty: newInventory,
+				product_sales: saleAmount
+			},
+			{
+				product_id: prodCode
+			}
+		],
+		function (err, res) {
+			if (err) throw err;
+			console.log("Your order has been placed. Your card on file will be charged $ " + saleAmount);
+			salesPrompt();
+		}
+	);
+}
+
+function salesPrompt() {
+	inquirer
+		.prompt([
+			{
+				type: "confirm",
+				message: "Do you want to continue shopping?:",
+				name: "confirm",
+				default: true
+			}
+		])
+		.then(function (shoppingResponse) {
+			if (shoppingResponse.confirm) {
+				getSaleOrder();
+			}
+			else {
+				console.log("\nThanks for shopping at Mamazon. Come back soon..\n");
+				connection.end();
+			}
+		});
+}
---
+++ mamazonDB.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS mamazonDB;
+CREATE DATABASE mamazonDB;
+USE mamazonDB;
+
+CREATE TABLE products(
+  product_id INTEGER not null AUTO_INCREMENT
+, product_name VARCHAR(100) not null
+, department_name VARCHAR(45)
+, price DECIMAL(10,2) NOT NULL
+, stock_qty INTEGER NOT NULL
+, product_sales DECIMAL(20,2)
+, PRIMARY KEY (product_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_qty)
+VALUES 
+ ("Wings of Fire","Books",14.50,2000)
+,("Laptop charger","Electronics",21.50,20)
+,("Tulip Bulbs","Garden",16.75,268)
+,("iPhone7","Electronics",998.00,365)
+,("Avacodo - Bag","Foods",10.00,875)
+,("Dining Table","Home Goods",699.99,130)
+,("Patio Set","Garden",899.99,65)
+,("Unwanteds","Books",16.25,9)
+,("Laptop charger","Electronics",21.50,20)
+,("Brooks Ghost 10 Sneakers","Sports",120.00,456)
+,("Eloquent JavaScript","Books",46.95,823)
+,("Priya Mixer Grinder","Home Goods",245.10,9)
+
+;
+
+
